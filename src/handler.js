379c61a8e4a88a4a23e6446d831c1a405137f9ec
Kyle Lin <lyhcode@gmail.com>
2016-06-20 18:20:08 +0800

    add signature

--- src/handler.js
+++ src/handler.js
@@ -1,72 +1,78 @@
-import { Handler, operation } from 'lambda6';
-
-var aws = require('aws-sdk');
-
-var elastictranscoder = new aws.ElasticTranscoder();
-
-function basename(path) {
-   return path.split('/').reverse()[0].split('.')[0];
+import {
+  Handler,
+  operation
 }
+from 'lambda6';
 
-function outputKey(name, ext) {
-   return name + '-' + Date.now().toString() + '.' + ext;
-}
+import AWS from 'aws-sdk';
+import crypto from 'crypto';
+import config from './config';
 
+const s3 = new AWS.S3();
+    
 export default class TestHandler extends Handler {
 
-  /**
-   * Sample operation handler for the "test" operation, it simply causes the
-   * handler to succeed with the the operation name and payload it's given.
-   * @param {Object} payload - the `payload` field extracted from `event`
-   */
   @operation
-  test(payload) {
-    // No need to call `this.context.succeed()`, just return the value
-    return { operationName: this.operation, value: payload };
+  echo(payload) {
+    return {
+      payload: payload
+    };
   }
+  
+  // @operation
+  // accessKey(payload) {
+  //   return {
+  //     accessKey: config.accessKey
+  //   }
+  // }
 
   @operation
   s3event(payload) {
     //var key = payload.Records[0].s3.object.key;
-    return { result: true };
+    return {
+      result: true
+    };
   }
 
   @operation
   nothing(payload) {
-    return { result: true };
-  }
-
-  @operation
-  echo(payload) {
-    return { payload: payload };
-  }
-
-  @operation
-  transcoder(payload) {
-
-    var key = payload.key;
-
-    var params = {
-      Input: { 
-        Key: key
-      },
-      PipelineId: '1462358425462-kxvzpj',
-      OutputKeyPrefix: 'emvpcontent/',
-      Outputs: [
-        {
-          Key: outputKey(basename(key), 'mp4'),
-          PresetId: '1465455390986-0t1jc6',
-        }
-      ]
+    return {
+      result: true
     };
-    
-    console.log('+++ Invoke elastictranscoder.createJob');
+  }
 
-    elastictranscoder.createJob(params, function(err, data) {
-      console.log("+++ elastictranscoder.createJob callback");
-      if (err){
-        console.log(err);
-      }
-    });
+  // @operation
+  // transcoder(payload) {
+  // }
+
+  @operation
+  signature(payload) {
+
+    const fileName = payload.conditions[5]['x-amz-meta-qqfilename'];
+    const fileType = payload.conditions[2]['Content-Type'];
+    const S3_BUCKET = payload.conditions[1]['bucket'];
+
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Key: fileName,
+      Expires: 60,
+      ContentType: fileType,
+      ACL: 'public-read'
+    };
+
+    let policy = new Buffer(JSON.stringify(payload)).toString('base64').replace(/\n|\r/, '');
+    let hmac = crypto.createHmac("sha1", config.secretAccessKey);
+
+    let hash2 = hmac.update(policy);
+    let signature = hmac.digest("base64");
+
+    let data = s3.getSignedUrl('putObject', s3Params);
+
+    return {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
+      policy: policy,
+      signature: signature
+    };
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,72 +1,35 @@
 import Handler from './handler';
-import aws from 'aws-sdk';
+import AWS from 'aws-sdk';
 
-const elastictranscoder = new aws.ElasticTranscoder();
+const elastictranscoder = new AWS.ElasticTranscoder();
 
-function basename(path) {
-   return path.split('/').reverse()[0].split('.')[0];
-}
-
-function outputKey(name, ext) {
-   return name + '-' + Date.now().toString() + '.' + ext;
-}
-
-/**
- * Re-export of `Handler.handle()` that will be exposed to AWS Lambda as the
- * entry point to use when this bundle is uploaded to AWS.
- * @param {Object} event - the AWS Lambda event
- * @param {Object} context - the AWS Lambda context
- */
 export function handler(event, context) {
 
+  console.log(event);
+
   if (event.operation == 'transcoder') {
-    var key = event.payload.key;
 
-    var params = {
-      Input: { 
-        Key: key
-      },
-      PipelineId: '1462358425462-kxvzpj',
-      OutputKeyPrefix: 'emvpcontent/',
-      Outputs: [
-        {
-          Key: outputKey(basename(key), 'mp4'),
-          PresetId: '1465455390986-0t1jc6',
-        }
-      ]
-    };
-    
-    console.log('+++ Invoke: elastictranscoder.createJob');
+    console.log('>>> Invoke: elastictranscoder.createJob');
 
-    elastictranscoder.createJob(params, function(err, data) {
+    elastictranscoder.createJob(event.payload, function(err, data) {
 
-      console.log("+++ Callback: elastictranscoder.createJob callback");
+      console.log("<<< Callback: elastictranscoder.createJob callback");
 
       if (err){
         console.log(err);
         context.fail();
         return;
       }
+      
       context.succeed();
     });
   }
   else if (event.operation && event.payload) {
     return new Handler().handle(event, context);
   }
-
-/*
-  if (!event.operation) {
-    if (event.Records) {
-      event.operation = 's3event';
-      event.payload = { Records: event.Records };
-    }
-    else {
-      event.operation = 'nothing';
-      event.payload = {};
-    }
+  else {
+    event.operation = 'nothing';
+    event.payload = {};
+    return new Handler().handle(event, context);
   }
-
-  return new Handler().handle(event, context);
-*/
-
 }
