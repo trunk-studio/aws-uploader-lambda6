e952e288353ffa8465c6994a4b9e42592525abe2
Kyle Lin <lyhcode@gmail.com>
2016-06-28 01:49:28 +0800

    callback done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Handler from './handler';
 import AWS from 'aws-sdk';
+import fetch from 'node-fetch';
+import FormData from 'form-data';
 
 const elastictranscoder = new AWS.ElasticTranscoder();
 
@@ -18,7 +20,54 @@ export function handler(event, context) {
       console.log('<<< Transcoder Job Callback <<<' + JSON.stringify(sns));
       //console.log(sns);
       
+      let message = JSON.parse(sns.Message);
+      let outputs = message.outputs;
+      let userMetadata = message.userMetadata;
       
+      let callbackEndpoint = userMetadata.CallbackEndpoint;
+      
+      let callbackParams = JSON.parse(
+        new Buffer(
+          userMetadata.CallbackParams0 + userMetadata.CallbackParams1 +
+          userMetadata.CallbackParams2 + userMetadata.CallbackParams3, 'base64'
+        ).toString('utf8')
+      );
+      
+      for (let i = 0; i < outputs.length; i++) {
+        let output = outputs[i];
+        
+        if (output.height == 480) {
+          callbackParams.videoSize480 = '';
+          callbackParams.thumbnail480 = '';
+        }
+        else if (output.height == 720) {
+          callbackParams.videoSize720 = '';
+          callbackParams.thumbnail480 = '';
+        }
+        
+        callbackParams.videoDuration = output.duration;
+      }
+
+      /*
+      console.log((callbackParams));
+      console.log(JSON.stringify(callbackParams));
+      */
+
+      // Use httpbin.org for testing purpose
+      // 'http://httpbin.org/post'
+      
+      fetch(callbackEndpoint, {
+        method: 'POST',
+        body: JSON.stringify(callbackParams),
+      	redirect: 'follow',
+      	headers: {'Content-Type': 'application/json'}
+      })
+      .then(function(res) {
+          return res.json();
+      }).then(function(json) {
+          console.log(json);
+          context.succeed();
+      });
     }
   }
   else if (event.operation == 'transcoder') {
